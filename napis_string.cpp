8aa9f5659e16ba1b660c479d139295c06ee224b8
RadoslawP <radoslaw.pietraszek.r@gmail.com>
2020-06-02 09:59:08 +0200

    stringi

---
+++ napis_string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string napis;
+    cout << "Podaj napis: " << endl;
+    getline(cin,napis);
+
+    cout<<napis;
+
+    return 0;
+}
